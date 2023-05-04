35ca633cd13bb5bdfe76439284cccff2a98c3a1a
Muneeb Aslam <muneeb.official.686332@hotmail.com>
2023-05-04 19:47:30 +0500

    Done

--- main.js
+++ main.js
@@ -24,3 +24,28 @@ const sliderObserver = new IntersectionObserver((slide)=>{
 sliderObserver.observe(slides[slides.length-1])
 
 slideBtns.forEach(btn => btn.addEventListener("click",handleClick))
+
+function fadeUpObserverCallback(elsToWatch){
+    elsToWatch.forEach((el) => {
+      if(el.isIntersecting){
+        el.target.classList.add('faded');
+        fadeUpObserver.unobserve(el.target);
+        el.target.addEventListener("transitionend", () => {
+          el.target.classList.remove('fade-up', 'faded');
+        }, { once: true })
+      }
+    })
+  }
+  
+  const fadeUpObserverOptions = {
+    threshold: .6,
+  }
+  
+  const fadeUpObserver = new IntersectionObserver(
+    fadeUpObserverCallback,
+    fadeUpObserverOptions
+  );
+  
+  document.querySelectorAll('.fade-up').forEach((item) => {
+    fadeUpObserver.observe(item);
+  })
